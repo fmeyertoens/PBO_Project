80b83274f4e4d1a4f6472c5e0e846c282ccef19d
Florian Fricke <s74003@informatik.htw-dresden.de>
2018-01-24 13:34:08 +0100

    add main modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProcessComponent } from './process/process.component';
+import { StakeholderComponent } from './stakeholder/stakeholder.component';
+import { LocationComponent } from './location/location.component';
+import { ListProcessComponent } from './process/list-process/list-process.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProcessComponent,
+    StakeholderComponent,
+    LocationComponent,
+    ListProcessComponent
   ],
   imports: [
     BrowserModule
