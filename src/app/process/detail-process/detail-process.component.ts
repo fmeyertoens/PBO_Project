a1466e189feb5b5ac2bb2dcabb33f63bf422604c
Florian Fricke <s74003@informatik.htw-dresden.de>
2018-01-27 20:08:18 +0100

    add list-process and process-detail

--- src/app/process/detail-process/detail-process.component.ts
+++ src/app/process/detail-process/detail-process.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { ProcessService } from './../../process.service';
+import { Process } from './../process';
 
 @Component({
   selector: 'app-detail-process',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-process.component.scss']
 })
 export class DetailProcessComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private processService: ProcessService,
+    private location: Location
+  ) { }
 
-  constructor() { }
+  @Input() process: Process;
 
-  ngOnInit() {
+  getProcessDetail(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.processService.getSingleProcess(id)
+      .subscribe(process => this.process = process);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  ngOnInit(): void {
+    this.getProcessDetail();
+  }
 }
--- src/app/process/list-process/list-process.component.ts
+++ src/app/process/list-process/list-process.component.ts
@@ -1,4 +1,7 @@
+import { ProcessService } from './../../process.service';
 import { Component, OnInit } from '@angular/core';
+import { ProcessMock } from '../mock-process';
+import { Process } from '../process';
 
 @Component({
   selector: 'app-list-process',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-process.component.scss']
 })
 export class ListProcessComponent implements OnInit {
+  tablehead = ['Bezeichnung', 'Prozessort', 'Prozessinitiator'];
+  // tabledata = ProcessMock;
+  selectedProcess: Process;
+  tabledata: Process[]; // Array von Service
 
-  constructor() { }
+  getProcess(): void {
+    this.processService.getProcess()
+      .subscribe(process => this.tabledata = process);
+    // suscribe wartet auf die Antwort/ Heros des Server
+  }
+
+  // onSelect(hero: Hero): void {
+  //   console.log('klick');
+  //   this.selectedHero = hero;
+  // }
+
+  // private, weil ich die var. des Service im Template nicht verwende
+  constructor(private processService: ProcessService) { }
 
   ngOnInit() {
+    this.getProcess();
   }
+
 
 }
