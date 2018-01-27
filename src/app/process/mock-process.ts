33675cfac8e6d17b6548f15ff061bdfb52d7bebf
Florian Fricke <s74003@informatik.htw-dresden.de>
2018-01-27 20:06:48 +0100

    add mock data

---
+++ src/app/process/mock-process.ts
@@ -0,0 +1,40 @@
+import { Process } from './process';
+
+export const ProcessMock: Process[] = [
+  new Process (
+    1,
+     'Prozess1',
+     'Dresden',
+     'Prof. Freitag',
+  ),
+  new Process(
+    2,
+    'Prozess2',
+    'Leipzig',
+    'Prof. Sonntag',
+  ),
+  new Process(
+    3,
+    'Prozess3',
+    'Chemnitz',
+    'Prof. Montag',
+  ),
+  new Process(
+    4,
+    'Prozess4',
+    'Freiberg',
+    'Prof. Mittwoch',
+  ),
+  new Process(
+    5,
+    'Prozess5',
+    'Flöha',
+    'Prof. Donnerstag',
+  ),
+  new Process(
+    9,
+    'Prozess6',
+    'Dresden',
+    'Prof. Freitag',
+  ),
+];
---
+++ src/app/process/process.ts
@@ -0,0 +1,9 @@
+export class Process {
+  constructor(
+  public id: number,
+  public name: string,
+  public location: string,
+  public initiator: string,
+  ) {}
+}
+
