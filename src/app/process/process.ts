9caa5c0509deb2ef015ac73ca82844435eea185e
Florian Fricke <s74003@informatik.htw-dresden.de>
2018-02-23 12:02:45 +0100

    change json

--- src/app/process/process.ts
+++ src/app/process/process.ts
@@ -4,8 +4,8 @@ export class Process {
   public name: string,
   public location: string,
   public initiator: string,
-  public description: string,
   public participation: string,
+  public description: string,
   public parent: string,
   ) {}
 }
